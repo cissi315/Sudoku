84b2c0372f50183352faebc63a91353cab59ca68
elsareuter <91029437+elsareuter@users.noreply.github.com>
2022-02-21 10:51:58 +0100

    Update Sudoku.hs

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,3 +1,24 @@
+{- List representing a sudoku board.
+   9 Rows each containing 9 cells.
+   INVARIANT: must have exactly 9 Rows
+              all rows must have same length
+ -}
+type Grid = [Row] 
+
+{- List representing a horisontal row in sudoku board.
+   A row has 9 numbers.
+   INVARIANT: length must be exactly 9
+ -}
+type Row = [Cell]
+
+{- Represents a cell in a sudoku board
+   List of possible numbers
+   INVARIANT: numbers are 1-9
+              a number can not appear twice
+ -}
+type Cell = [Int]
+
+
 {-
 -}
 data SudokuValue = Int deriving (Read, Show, Enum, Eq, Ord)
