bf2e304196e540ecc2ca2dc3bd81c21cd7755cde
cissi315 <cissi315@DESKTOP-0FKSLR3.localdomain>
2022-02-21 09:44:35 +0100

    init

---
+++ Sudoku.hs
@@ -0,0 +1,24 @@
+{-
+-}
+data SudokuValue = Int deriving (Read, Show, Enum, Eq, Ord)
+type Board = [[SudokuValue]]
+
+solve :: [[a]] -> [[a]]
+solve boardempty  
+
+{-main = do  
+    putStrLn "Insert Soduko"  
+    sudokuinput <- getLine  
+    putStrLn (sudokuinput)-}
+
+boardempty = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
