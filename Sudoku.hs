9342ef9c22ea98d934e24c4158eb30c72560b311
cissi315 <cecilia.lindeborg@gmail.com>
2022-02-23 11:59:28 +0100

    Example Board

--- Sudoku.hs
+++ Sudoku.hs
@@ -9,7 +9,7 @@ type Board = [[Int]]
     sudokuinput <- getLine  
     putStrLn (sudokuinput)-}
 
-boardempty :: [[Integer]]
+boardempty :: Board
 boardempty = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,29 +23,21 @@ boardempty = [
     ]
 
 example :: Board
-example = [[0,0,0,0,0,0,0,0,0],
-           [0,4,6,1,8,2,5,9,3],
-           [3,2,0,9,5,6,4,8,7],
-           [7,3,5,4,0,1,9,6,0],
-           [0,6,1,0,9,8,7,3,4],
-           [4,0,9,5,3,7,8,1,2],
-           [2,9,8,0,7,4,6,5,1],
-           [1,5,4,0,0,9,2,7,8],
-           [8,7,0,6,2,5,3,4,9]]
---solve ::
+example = [[0,3,0,8,2,0,0,1,0],
+           [0,6,1,4,5,7,8,2,3],
+           [8,7,0,0,0,1,0,0,0],
+           [2,4,8,0,0,3,0,5,7],
+           [6,1,0,2,8,0,0,9,0],
+           [0,0,0,7,4,0,0,8,0],
+           [4,0,3,5,7,8,9,6,0],
+           [1,0,5,0,0,0,4,7,0],
+           [0,8,0,1,0,4,0,3,5]]
+
+solve :: Board -> Board
+solve board = solve' board  
 
 checkRow :: Board -> Int -> Int -> Bool
 checkRow board row value = value `notElem` (board !! (row - 1))
-
-{-
-checkColumn1 :: Board -> Int -> Int -> Bool
-checkColumn1 board column value = checkColumn' board 0 column value
-    where 
-        checkColumn' board 9 column value = True
-        checkColumn' board row column value 
-            | value == (board !! row !! column) = False
-            | otherwise = checkColumn' board (row+1) column value 
--}
 
 checkColumn :: Board -> Int -> Int -> Bool
 checkColumn board column value = value `notElem` checkColumn' board column 
@@ -66,3 +58,14 @@ checkBox board row column value
     | otherwise = value `notElem` checkBox' board 7 7
         where
             checkBox' board row column = take 3 (drop (column-1) (board !! (row - 1))) ++ take 3 (drop (column-1) (board !! row)) ++ take 3 (drop (column-1) (board !! (row+1)))
+
+--Alternativ
+{-
+checkColumn1 :: Board -> Int -> Int -> Bool
+checkColumn1 board column value = checkColumn' board 0 column value
+    where 
+        checkColumn' board 9 column value = True
+        checkColumn' board row column value 
+            | value == (board !! row !! column) = False
+            | otherwise = checkColumn' board (row+1) column value 
+-}
