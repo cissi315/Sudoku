265d2fa1e8bcfb607dc60f340410ef9f1086b3af
cissi315 <cecilia.lindeborg@gmail.com>
2022-03-03 10:18:53 +0100

    PrintBoard and IO

--- Sudoku.hs
+++ Sudoku.hs
@@ -1,13 +1,9 @@
 --import Gistfile1
-
-type Board = [[Int]]
-
 --source = Gistfile1.main
 
-{-main = do  
-    putStrLn "Insert Soduko"  
-    sudokuinput <- getLine  
-    putStrLn (sudokuinput)-}
+module Sudoku(solve, insert, validMove) where 
+
+type Board = [[Int]]
 
 boardempty :: Board
 boardempty = [
@@ -24,8 +20,8 @@ boardempty = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
 
-example :: Board
-example = [[0,3,0,8,2,0,0,1,0],
+exampleEasy :: Board
+exampleEasy = [[0,3,0,8,2,0,0,1,0],
            [0,6,1,4,5,7,8,2,3],
            [8,7,0,0,0,1,0,0,0],
            [2,4,8,0,0,3,0,5,7],
@@ -35,10 +31,51 @@ example = [[0,3,0,8,2,0,0,1,0],
            [1,0,5,0,0,0,4,7,0],
            [0,8,0,1,0,4,0,3,5]]
 
-{- 
+exampleEasyFinish :: Board
+exampleEasyFinish = [
+           [5,3,4,8,2,9,7,1,6],
+           [9,6,1,4,5,7,8,2,3],
+           [8,7,2,3,6,1,5,4,9],
+           [2,4,8,9,1,3,6,5,7],
+           [6,1,7,2,8,5,3,9,4],
+           [3,5,9,7,4,6,1,8,2],
+           [4,2,3,5,7,8,9,6,1],
+           [1,9,5,6,3,2,4,7,8],
+           [7,8,6,1,9,4,2,3,5]]
+
+exampleHard :: Board
+exampleHard = [
+           [0,0,8,1,5,0,9,0,2],
+           [0,2,0,0,0,0,0,5,0],
+           [0,0,0,0,2,0,1,6,4],
+           [0,8,0,6,0,0,0,0,0],
+           [0,0,3,4,0,8,2,0,0],
+           [0,0,0,0,0,9,0,1,0],
+           [3,5,9,0,8,0,0,0,0],
+           [0,4,0,0,0,0,0,2,0],
+           [2,0,6,0,9,4,3,0,0]]
+
+
 solve :: Board -> Board
-solve board = solve' board  
--}
+solve board 
+    | find0 board == (9,0) = board
+    | otherwise = solve' board (find0 board) 1
+    where
+        solve' board (row,column) 10 = [[]]
+        solve' board (row,column) value
+            | checkRules board row column value && x /= [[]] = x
+            | otherwise = solve' board (row,column) (value+1)
+                where
+                    x = solve (insert board row column value)
+
+insert :: Board -> Int -> Int -> Int -> Board
+insert [] _ _ _ = []
+insert board row column value = take (row - 1) board ++ insertAux (board !! (row-1)) column value : drop row board
+    where insertAux :: [Int] -> Int -> Int -> [Int]
+          insertAux [] _ _ = []
+          insertAux (x:xs) c value -- vilken motsvarar row mm?
+               | checkRules board row column value = take (c - 1) (x:xs) ++ value : drop c (x:xs)
+               | otherwise = x:xs
 
 checkRow :: Board -> Int -> Int -> Bool
 checkRow board row value = value `notElem` (board !! (row - 1))
@@ -66,8 +103,48 @@ checkBox board row column value
 checkRules :: Board -> Int -> Int -> Int -> Bool
 checkRules board row column value = checkBox board row column value && checkColumn board column value && checkRow board row value 
 
+{-
+   EXAMPLES: find0 exampleFinish == (9,0)
+ -}
+find0 :: Board -> (Int, Int)
+find0 board = (row board, column (board !! (row board - 1)))
+    where
+        row :: Board -> Int
+        row [] = 0
+        row board
+            | 0 `elem` (board !! 0) = 1 
+            | 0 `elem` (board !! 1) = 2
+            | 0 `elem` (board !! 2) = 3
+            | 0 `elem` (board !! 3) = 4
+            | 0 `elem` (board !! 4) = 5
+            | 0 `elem` (board !! 5) = 6
+            | 0 `elem` (board !! 6) = 7
+            | 0 `elem` (board !! 7) = 8
+            | otherwise = 9
+
+        column :: [Int] -> Int 
+        column [] = 0
+        column (x:xs) 
+            | x == 0 = 9 - length xs
+            | otherwise = column xs
+
+validMove :: Board -> Int -> Int -> Int -> Bool
+validMove board row column value
+    | (board !! (row-1) !! (column-1)) /= 0 = False 
+    | otherwise = validMove' (solve board) row column value
+        where
+            validMove' board row column value 
+                | (board !! (row-1) !! (column-1)) == value = True 
+                | otherwise = False
 
 --Alternativ
+
+{-main :: IO ()
+main = do  
+    putStrLn "Insert Soduko"  
+    sudokuinput <- getLine  
+    putStrLn (sudokuinput)-}
+
 {-
 checkColumn1 :: Board -> Int -> Int -> Bool
 checkColumn1 board column value = checkColumn' board 0 column value
@@ -78,25 +155,5 @@ checkColumn1 board column value = checkColumn' board 0 column value
             | otherwise = checkColumn' board (row+1) column value 
 -}
 
-
-find0 :: Board -> (Int, Int)
-find0 board = (row board, column (board !! (row board - 1)))
-
-row :: Board -> Int
-row [] = 0
-row board
-  | 0 `elem` (board !! 0) = 1 
-  | 0 `elem` (board !! 1) = 2
-  | 0 `elem` (board !! 2) = 3
-  | 0 `elem` (board !! 3) = 4
-  | 0 `elem` (board !! 4) = 5
-  | 0 `elem` (board !! 5) = 6
-  | 0 `elem` (board !! 6) = 7
-  | 0 `elem` (board !! 7) = 8
-  | otherwise = 9
-
-column :: [Int] -> Int 
-column [] = 0
-column (x:xs) 
-        | x == 0 = 9 - length xs
-        | otherwise = column xs
+--replace :: Int -> a -> [a] -> [a]
+--replace column value board = take column board ++ [value] ++ drop (column+1) board
