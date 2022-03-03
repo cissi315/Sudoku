cd4a99b6ef2f0ccf11d912f6c374afa0d49c80b2
cissi315 <cecilia.lindeborg@gmail.com>
2022-03-03 12:21:55 +0100

    BoardExample and Difficulty

--- SudokuIO.hs
+++ SudokuIO.hs
@@ -1,56 +1,7 @@
 import Sudoku
+import SudokuExample
 
 type Board = [[Int]]
-
-boardempty :: Board
-boardempty = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-
-exampleEasy :: Board
-exampleEasy = [[0,3,0,8,2,0,0,1,0],
-           [0,6,1,4,5,7,8,2,3],
-           [8,7,0,0,0,1,0,0,0],
-           [2,4,8,0,0,3,0,5,7],
-           [6,1,0,2,8,0,0,9,0],
-           [0,0,0,7,4,0,0,8,0],
-           [4,0,3,5,7,8,9,6,0],
-           [1,0,5,0,0,0,4,7,0],
-           [0,8,0,1,0,4,0,3,5]]
-
-exampleEasyFinish :: Board
-exampleEasyFinish = [
-           [5,3,4,8,2,9,7,1,6],
-           [9,6,1,4,5,7,8,2,3],
-           [8,7,2,3,6,1,5,4,9],
-           [2,4,8,9,1,3,6,5,7],
-           [6,1,7,2,8,5,3,9,4],
-           [3,5,9,7,4,6,1,8,2],
-           [4,2,3,5,7,8,9,6,1],
-           [1,9,5,6,3,2,4,7,8],
-           [7,8,6,1,9,4,2,3,5]]
-
-exampleHard :: Board
-exampleHard = [
-           [0,0,8,1,5,0,9,0,2],
-           [0,2,0,0,0,0,0,5,0],
-           [0,0,0,0,2,0,1,6,4],
-           [0,8,0,6,0,0,0,0,0],
-           [0,0,3,4,0,8,2,0,0],
-           [0,0,0,0,0,9,0,1,0],
-           [3,5,9,0,8,0,0,0,0],
-           [0,4,0,0,0,0,0,2,0],
-           [2,0,6,0,9,4,3,0,0]]
 
 stringNumber :: Int -> String
 stringNumber 0 = "_"
@@ -101,6 +52,18 @@ game board = do
             game board
     
 main = do  
-    putStrLn "Let's play Sudoku, choose a difficulty."  
-    sudokuinput <- getLine  
-    game exampleEasy 
+    putStrLn "Let's play Sudoku, choose a difficulty. 1 = Easy, 2 = Medium, 3 = Hard, 4 = Expert. "  
+    sudokudifficulty <- getLine
+    if sudokudifficulty == "1" then do
+         game SudokuExample.boardEasy
+    else if sudokudifficulty == "2" then do
+         game SudokuExample.boardMedium
+    else if sudokudifficulty == "3" then do
+         game SudokuExample.boardHard
+    else if sudokudifficulty == "4" then do
+         game SudokuExample.boardExpert
+    else if sudokudifficulty == "5" then do -- Test
+         game SudokuExample.boardEasyFinish
+    else do
+        putStrLn "Wrong input. Try again."
+        main
